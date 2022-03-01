0b5debac4cf032784a89f61731a6a24220564a78
giagra91 <giagra91@gmail.com>
2022-03-01 15:13:37 +0100

    inizializzazione carousel

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const images = [`01.jpg`, `02.jpg`, `03.jpg`, `04.jpg`, `05.jpg`];
+const places = [`Svizzera`, `Lago di Como`, `Firenze`, `Spiaggia`, `Mare`];
+const news = [`Lorem Ipsum`, `Lorem Ipsum`, `Lorem Ipsum`, `Lorem Ipsum`, `Lorem Ipsum`];
